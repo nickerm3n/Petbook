a65cd9e9267167a98e6b679d0bb9af8161031a89
Nikita Kurilko1 <nikita.kurilko1@studio-moderna.com>
2019-06-10 22:36:17 +0300

    add advert page

--- app/js/main.js
+++ app/js/main.js
@@ -3,6 +3,29 @@
 (function($){
 	$(document).ready(function() {
 		// Code
+		checkWidth();
 
+		$( window ).resize(()=> {
+			toDrag()
+		})
+
+		$( "#tabs" ).tabs();
+
+		$('.tabs__link').click(function() {
+			$('.tabs__link').removeClass('active');
+			$(this).addClass('active');
+		})
 	});
 })(jQuery);
+
+
+function toDrag() {
+	let tabs = $('#tabs'),
+		menu = $('.navigation--main'),
+		container = $('.petbook-container');
+	($( window ).width() > 415 ? menu.append(tabs) : container.append(tabs))
+}
+
+function checkWidth() {
+	$( window ).width() > 415 ? toDrag() : null;
+}
